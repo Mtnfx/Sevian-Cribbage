625385f672a569d033b4874d5ced9c3fe6a2d9d1
Mtnfx <alec.larsen521@gmail.com>
2026-01-17 20:57:26 -0500

    core: add Player, Opponent classes

---
+++ core/player.py
@@ -0,0 +1,21 @@
+from .hand import Hand
+
+class Player:
+    name: str #Display name of player.
+    hand: Hand #Current hand of player.
+    points: int #Number of points the player has in the current game.
+    
+    def __init__(self, name: str):
+        self.name = name
+        self.hand = Hand([])
+        self.points = 0
+        
+    def throw_card(self, index: int, crib: Hand) -> None:
+        """
+        Throw card from player's hand into crib.
+
+        Args:
+            index (int): Index of card to b thrown into crib.
+            crib (Hand): Current state of crib.
+        """
+        crib += self.hand.cards.pop(index)
---
+++ tests/test_player.py
@@ -0,0 +1,45 @@
+from core.deck import Deck
+from core.hand import Hand
+from core.player import Player
+
+import pytest, random
+from collections.abc import Callable
+
+#Generate random hand of six cards and a crib of two cards from Sevian deck. Also randomly select an index from 0 to 5.
+def player_crib_pair():
+    tmp_deck = Deck(13,5)
+    pl = Player("Anelace Foxglove")
+    pl.hand = Hand(tmp_deck.cards[0:6])
+    return (pl, Hand(tmp_deck.cards[6:8]), random.randint(0,5))
+
+#Verify that player hand length is modified by throw_card
+@pytest.mark.parametrize("pair_crib_factory", [
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+])
+
+def test_throw_card_hand(pair_crib_factory: Callable[[], tuple[Player, Hand, int]]):
+    pl, crib, i = pair_crib_factory()
+    init_len = len(pl.hand) #Save initial size of player hand for comparison
+    pl.throw_card(i, crib)
+    assert len(pl.hand) == init_len - 1 #Player's hand should now have one less card than before.
+
+#Verify that crib length is modified by throw_card
+@pytest.mark.parametrize("pair_crib_factory", [
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+    player_crib_pair,
+])
+
+def test_throw_card_crib(pair_crib_factory: Callable[[], tuple[Player, Hand, int]]):
+    pl, crib, i = pair_crib_factory()
+    init_len = len(crib) #Save initial size of crib for comparison.
+    pl.throw_card(i, crib)
+    assert len(crib) == init_len + 1 #Crib should now contain one more card than before.
