1c714c59f5dd3609156da395f898ef33597aa3e9
Mtnfx <alec.larsen521@gmail.com>
2026-01-22 13:22:28 -0500

    opponent: add generation of random decision matrix

--- core/opponent.py
+++ core/opponent.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from .hand import Hand
 from .player import Player
 
@@ -10,7 +12,28 @@ class Opponent(Player):
     behaviour: list[int] #Weightings for types of decision methods opponent uses to pick which cards to throw away.
     dialogue: list[str] #Set of dialogue lines that opponent may say during play.
 
-    def __init__(self, name: str, behaviour: list[int] = [], dialogue: list[str] = []): #pylint: disable=dangerous-default-value
+    def __init__(self, name: str, behaviour: list[int] = [1], dialogue: list[str] = NotImplemented): #pylint: disable=dangerous-default-value
         super().__init__(name)
         self.behaviour = behaviour
         self.dialogue = dialogue
+
+    #Decision matrix generators
+    def randomDecisionMatrix(self) -> np.typing.NDArray[np.float64]:
+        """
+        Generate decision matrix for choosing random card to throw into crib.
+        
+        Will return a vector if one one card is to be thrown and a matrix if two cards must be thrown.
+        
+        Assumptions:
+            - player hand (self.hand) has a size of 5 or 6
+
+        Returns:
+            np.ndarray: Decision matrix for throwing card into crib.
+        """
+        #If hand is of size 5, only one card needs to be thrown.
+        if len(self.hand) == 5:
+            return np.ones(5)/5
+
+        #Else, hand is of size 6 and we must throw away two cards.
+        else:
+            return np.ones(shape=(6,6))/15
